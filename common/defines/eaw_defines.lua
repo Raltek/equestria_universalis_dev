6759e5bba995791466c7fcfc854066911bfe0e34
cijik <cijik1@gmail.com>
2020-04-06 20:42:06 +0300

    Fixed extra title frame, extracted modified defines to new file

---
+++ common/defines/eaw_defines.lua
@@ -0,0 +1,13 @@
+NDefines.NFrontend.CAMERA_LOOKAT_X = 2690.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 2690.0
+NDefines.NFrontend.CAMERA_START_X = 2690.0
+NDefines.NFrontend.CAMERA_END_X = 2690.0
+NDefines.NFrontend.FRONTEND_POS_X = 2690.0
+NDefines.NFrontend.FRONTEND_POS_Y = 1500.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 2690.0
+NDefines.NFrontend.SETTINGS_POS_X = 2690.0
+NDefines.NFrontend.SETTINGS_POS_Y = 951.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2758.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1246.0
+NDefines.NFrontend.MP_OPTIONS_POS_X = 2690.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 2690.0
